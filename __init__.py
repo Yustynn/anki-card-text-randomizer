f866d2def7193244d6b0288fad06cdd78be18dc9
Yustynn Panicker <yustynn@gmail.com>
2024-06-03 08:28:08 +0700

    Finish order

--- __init__.py
+++ __init__.py
@@ -16,9 +16,56 @@ ORDER_ITEM_END = '}oi}'
 ORDER_CONTEXT_KEY = 'Alt+Shift+o'
 ORDER_ITEM_CONTEXT_KEY = 'Alt+Shift+i'
 
-
 def order(text):
-    pass
+    i = 0
+    while i < len(text):
+        if not text[i:i+len(ORDER_START)] == ORDER_START:
+            i += 1
+            continue
+
+        # remove ORDER_START
+        text = text[:i] + text[i+len(ORDER_START):]
+
+        # remove whitespace
+        ws = 0
+        while text[i + ws].isspace():
+            ws += 1
+        text = text[:i] + text[i+ws:]
+
+        cs: tuple[int, str] = [] # [(start idx, raw string)]
+        c_start = i
+        offset = 0
+        while i < len(text):
+            startswith = text[i:].startswith
+
+            # recurse if ORDER_START again
+            if startswith(ORDER_START):
+                text = text[:i] + order(text[i:]).strip()
+            # start new element
+            elif startswith(ORDER_ITEM_START):
+                c_start = i
+                text = text[:i] + text[i+len(ORDER_ITEM_START):]
+
+            # end new element
+            elif startswith(ORDER_ITEM_END):
+                cs.append((c_start, text[c_start:i]))
+                text = text[:i] + text[i+len(ORDER_ITEM_END):]
+            # reorder
+            elif startswith(ORDER_END):
+                text = text[:i] + text[i+len(ORDER_END):]
+                order = [c[1].strip() for c in cs]
+                shuffle(order)
+                for el1, (idx, el2) in zip(order, cs):
+                    l1 = len(el1)
+                    l2 = len(el2)
+                    text = text[:idx+offset] + el1 + text[idx+l2+offset:]
+                    offset += l1 - l2
+                break
+            else:
+                i += 1
+
+    return text
+
 
 
 def replace(text):
@@ -104,7 +151,7 @@ def addButtons(buttons, editor):
         "iconpath",
         "wrapOrderItem",
         wrapOrderItem,
-        tip=f"Wrap selection in an order context ({ORDER_ITEM_CONTEXT_KEY})",
+        tip=f"Wrap selection in an order item ({ORDER_ITEM_CONTEXT_KEY})",
         keys=ORDER_ITEM_CONTEXT_KEY
     ))
 
